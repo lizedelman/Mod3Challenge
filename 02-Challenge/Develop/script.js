351e1e042d30a4b7bc5bd670cca94dbb998c1d12
Wild Carrot <edelman.eliz@gmail.com>
2022-10-23 11:21:45 -0500

    Write list of tasks

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,14 +1,25 @@
+/* 
+1. On click I will have a series of prompts and have to store the input.
+  a. prompt for length
+  b. prompt to include lowercase
+  c. prompt to include uppercase
+  d. prompt to include numeric
+  e. prompt to include special characters
+2. Validate answers at each step
+3. Generate password
+*/
+
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+// prompt("Choose password length between 8 and 128 characters.");
